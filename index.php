30e45fc62a54938b70e3ee2ded8c132e54008c04
Alexandru Neacsu <dark2y@gmail.com>
2012-10-27 23:58:32 +0300

    Redone Stuff

--- index.php
+++ index.php
@@ -1,16 +1,1 @@
-<?php
-
-session_start();
-
-include "includes/config.php";
-
-$subpage = isset($_GET['p']) ? $_GET['p'] : "index";
-$isModal = isset($_GET['modal']) ? true : false;
-$session = $_SESSION;
-
-$page_path = "pages:$subpage.html.twig";
-if($isModal) $page_path = "dialogs:$subpage.html.twig";
-
-twig_render($page_path,array("page"=>$subpage,"server" => $_SERVER, "request" => $_REQUEST, "session" => $session));
- 
-?>
+test
