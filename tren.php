5fac4dc4607b0de4c346abf279dec8f8be87cdf6
laur3d <laurentiu3d@gmail.com>
2012-10-28 04:46:30 +0200

    X

--- tren.php
+++ tren.php
@@ -18,27 +18,40 @@
 
 			foreach($results as $result){
 
-				$tip
-				$nr
-				$plecare
-				$statie_plecare
-				$sosire
-				$statie_sosire
+				if(!is_null($result->Tren->Itren)){
+					$tip 					= $result->Tren->Itren->attributes()->tip->__toString();
+				}
+				if(isset($result->Tren->Itren)){
+					$nr 					= $result->Tren->Itren->attributes()->nr->__toString();
+				}
+				if(isset($result->Tren->Plecare)){
+					$plecare 				= $result->Tren->Plecare->attributes()->ora->__toString();
+				}
+				if(isset( $result->Tren->Plecare)){
+					$statie_plecare 		= $result->Tren->Plecare->attributes()->sta->__toString();
+				}
+				if(isset($result->Tren->Sosire)){
+					$sosire 				= $result->Tren->Sosire->attributes()->ora->__toString();
+				}
+				if(isset($result->Tren->Sosire)){
+					$statie_sosire 			= $result->Tren->Sosire->attributes()->sta->__toString();
+				}
+				if(isset($result->Tren->Asteptare)){
+					$asteptare = $result->Tren->Asteptare->attributes()->min->__toString();
+				}
 
 				$retArr[]  = array(
-									'tip' =>  $result->Tren->Itren->attributes()->tip->__toString(),
-									'nr' => $result->Tren->Itren->attributes()->nr->__toString(),
-									'plecare' => $result->Tren->Plecare->attributes()->ora->__toString(),
-									'statie_plecare' => $result->Tren->Plecare->attributes()->sta->__toString(),
-									'sosire' => $result->Tren->Sosire->attributes()->ora->__toString(),
-									'statie_sosire' => $result->Tren->Sosire->attributes()->sta->__toString(),
-									//'asteptare' => $result->Tren->Asteptare->attributes()->min->__toString(),
-
-
+									'tip' 				=> $tip,
+									'nr' 				=> $nr,
+									'plecare' 			=> $plecare,
+									'statie_plecare' 	=> $statie_plecare,
+									'sosire' 			=> $sosire,
+									'statie_sosire' 	=> $statie_sosire,
+									'asteptare' 		=> $asteptare
 
 
 				 );
-				print_r($retArr);
+				//print_r($retArr);
 			}
 			print_r($retArr);
 			//echo json_encode($result);
