b5c1ab7add6e63784e1f23b1d1d4a9f34aeb2df8
necenzurat <necenzurat@gmail.com>
2012-10-28 09:45:58 +0200

    api trenuri json

--- api.php
+++ api.php
@@ -7,7 +7,7 @@ $type = mysql_real_escape_string(@$_REQUEST["type"]);
 $to = mysql_real_escape_string(@$_REQUEST["to"]);
 $from = mysql_real_escape_string(@$_REQUEST["from"]);
 
-echo "callback{";
+echo "callback(";
 if ($type == "tren"){
 $xml = cache_url('http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%0A%20%20%20%20where%20url%3D\'http%3A%2F%2Fmerstrenuri.ro%2FLmb%3DXml%3FPle%3D' . $to . '%26Sos%3D' . $from . '%26Via%3D%26Sub%3DRute%26Tpe%3Don%26Tra%3Don%26Tin%3Don%26Ast%3D3%26Dac%3D15590\'&format=json&callback=', FALSE);
 $json = json_decode($xml, true);
@@ -15,7 +15,7 @@ $shittyjson = $json["query"]["results"]["Rute"]["Ruta"];
 echo json_encode($shittyjson);
 }
 
-echo "}";
+echo ")";
 
 /*
 
